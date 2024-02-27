502f3a38c041cb7f9e24d2efdec1f01bb9940c8c
Santiago León <santiago.leon@epn.edu.ec>
2024-02-27 18:56:09 +0000

    Uploaded project from local repository

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,63 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+  const units = {
+    gal: ["L", "gallons", 3.78541],
+    L: ["gal", "liters", 0.264172],
+    mi: ["km", "miles", 1.60934],
+    km: ["mi", "kilometers", 0.621373],
+    kg: ["lbs", "kilograms", 2.2046244],
+    lbs: ["kg", "pounds", 0.453592]
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  const formatter = new Intl.NumberFormat("en-US", {
+    minimumFractionDigits: 5,
+    maximumFractionDigits: 5
+  });
+
+  this.getNum = function (input) {
+    var arr = input.match(/\d*\.?\d+(\/\d*\.?\d+)?/g);
+    if (arr === null) {
+      return 1;
+    } else if (arr.length > 1) {
+      return undefined;
+    } else {
+      var num = arr[0];
+    }
+    if (num.match(/\//g)) {
+      let digits = num.split("/");
+      num = digits[0] / digits[1];
+    }
+    return Number(num);
   };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
+
+  this.getUnit = function (input) {
+    var uni = input.replace(/[^\p{Letter}]+/giu, "").toLowerCase();
+    if (uni === "l") {
+      uni = "L";
+    }
+    if (units[uni] === undefined) {
+      return undefined;
+    }
+    return uni;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+
+  this.getReturnUnit = function (initUnit) {
+    let returnUnit = units[initUnit];
+    return returnUnit === undefined ? undefined : returnUnit[0];
   };
-  
+
+  this.spellOutUnit = function (unit) {
+    return units[unit][1];
+  };
+
+  this.convert = function (initNum, initUnit) {
+    return Number(formatter.format(initNum * units[initUnit][2]));
+  };
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    return `${initNum} ${this.spellOutUnit(
+      initUnit
+    )} converts to ${returnNum.toFixed(5)} ${this.spellOutUnit(returnUnit)}`;
+  };
 }
 
 module.exports = ConvertHandler;
